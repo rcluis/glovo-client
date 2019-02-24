e3177740279de4e23b6ae1d399723e522916f0bf
rcluis <raulcatalanluis@gmail.com>
2019-02-24 14:17:41 +0100

    add axios dependency

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,61 +2,66 @@ const pkg = require('./package')
 
 
 module.exports = {
-  mode: 'universal',
-  server: {
-      port: 8000,
-  },
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-
-  /*
-  ** Global CSS
-  */
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/element-ui'
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-
-  /*
-  ** Build configuration
-  */
-  build: {
-    transpile: [/^element-ui/],
-    
+    mode: 'universal',
+    server: {
+        port: 8000,
+    },
     /*
-    ** You can extend webpack config here
+    ** Headers of the page
     */
-    extend(config, ctx) {
-      
+    head: {
+        title: pkg.name,
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: pkg.description}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ]
+    },
+
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: {color: '#fff'},
+
+    /*
+    ** Global CSS
+    */
+    css: [
+        'element-ui/lib/theme-chalk/index.css'
+    ],
+
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+        '@/plugins/element-ui'
+    ],
+
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        '@nuxtjs/axios',
+    ],
+
+    axios: {
+        baseUrl: 'http://localhost:3000'
+    },
+
+    /*
+    ** Build configuration
+    */
+    build: {
+        transpile: [/^element-ui/],
+
+        /*
+        ** You can extend webpack config here
+        */
+        extend(config, ctx) {
+
+        }
     }
-  }
 }
